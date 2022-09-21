52df714b7bb5f980a805cdd1027519665e254c0b
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-09-21 20:26:21 -0300

    update

---
+++ javascript/aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,2,9,3]
+
+
+num.push(98)
+num.sort()
+console.log(`Nosso vetor tem ${num.length} posições`);
+
+
+let pos = num.indexOf(9)
+console.log(`O valor 8 esta na posição ${pos}`);  
