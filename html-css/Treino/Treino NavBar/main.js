fddb778f1f593d1150fe809974be66a56a0354a3
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-11-02 16:05:37 -0300

    PMI-02/11

--- html-css/Treino/Treino NavBar/main.js
+++ html-css/Treino/Treino NavBar/main.js
@@ -2,8 +2,14 @@ const menubtn = document.querySelector('#menu-btn')
 const closebtn = document.querySelector('#close-btn')
 const navbar = document.querySelector('nav .container ul')
 
-menubtn.addEventListener('click', () => {
+menubtn.addEventListener('click', function() {
    navbar.style.display = 'block';
    menubtn.style.display = 'none';
    closebtn.style.display = 'inline-block';
 })
+
+ closebtn.addEventListener('click', () => {
+   navbar.style.display = 'none';
+   closebtn.style.display = 'none';
+   menubtn.style.display = 'inline-block';
+ })
