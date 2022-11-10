e9e1d541e9c4360750eea7c620d4111c914816a5
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-11-10 15:11:40 -0300

    Nav-bar Mobile finalizado

--- PMI/main.js
+++ PMI/main.js
@@ -1,3 +1,21 @@
+//MOSTRAR E ESCONDER NAV BAR NO MOBILE
+const menubtn = document.querySelector('#menu-btn')
+const closebtn = document.querySelector('#close-btn')
+const navbar = document.querySelector('#menu-bar')
+
+menubtn.addEventListener('click', function() {
+  navbar.style.display = 'block';
+  menubtn.style.display = 'none';
+  closebtn.style.display = 'inline-block';
+})
+
+closebtn.addEventListener('click', () => {
+  navbar.style.display = 'none';
+  closebtn.style.display = 'none';
+  menubtn.style.display = 'inline-block';
+})
+
+
 var swiper = new Swiper(".swiper", {
    spaceBetween: 30,
    centeredSlides: true,
