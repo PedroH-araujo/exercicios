56a3ec27651380bfb90a954bf1cb9709b57ed0e3
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-11-26 21:40:01 -0300

    finalizado

--- cursos/Alura/aluramidi/main.js
+++ cursos/Alura/aluramidi/main.js
@@ -1,51 +1,19 @@
-/*function tocaPom(){
-   document.querySelector('#som_tecla_pom').play()
+const tecla = document.querySelectorAll('.tecla')
+const som = document.getElementsByTagName('audio')
+
+for (let c = 0; c < tecla.length; c++) {
+   tecla[c].onclick = function(){
+      som[c].play()
+
+   tecla[c].onkeydown = function(evento){
+      if(evento.code === 'Space' || evento.code === 'Enter')
+      tecla[c].classList.add('ativa')
+   }
+   
+   tecla[c].onkeyup = function(){
+      tecla[c].classList.remove('ativa')
+   }
+
+   }
+
 }
-
-document.querySelector('.tecla_pom').onclick = tocaPom;
-
-var clap = document.querySelector('.tecla_clap')
-
-clap.addEventListener("click", function(){
-   document.getElementById('som_tecla_clap').play()
-})
-
-var tim = document.querySelector('.tecla_tim')
-
-tim.addEventListener("click", function(){
-   document.getElementById('som_tecla_tim').play()
-})
-
-var puff = document.querySelector('.tecla_puff')
-
-puff.addEventListener("click", function(){
-   document.getElementById('som_tecla_puff').play()
-})
-
-var splash = document.querySelector('.tecla_splash')
-
-splash.addEventListener("click", function(){
-   document.getElementById('som_tecla_splash').play()
-})
-
-var toim = document.querySelector('.tecla_toim')
-
-toim.addEventListener("click", function(){
-   document.getElementById('som_tecla_toim').play()
-})
-
-var psh = document.querySelector('.tecla_psh')
-psh.addEventListener("click", function(){
-   document.getElementById('som_tecla_psh').play()
-})
-
-var tic = document.querySelector('.tecla_tic')
-tic.addEventListener("click", function(){
-   document.getElementById('som_tecla_tic').play()
-})
-
-var tom = document.querySelector('.tecla_tom')
-
-tom.addEventListener("click", function(){
-   document.getElementById('som_tecla_tom').play()
-})*/
---
+++ cursos/Alura/aluramidi/teste.js
@@ -0,0 +1,51 @@
+/*function tocaPom(){
+   document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelector('.tecla_pom').onclick = tocaPom;
+
+var clap = document.querySelector('.tecla_clap')
+
+clap.addEventListener("click", function(){
+   document.getElementById('som_tecla_clap').play()
+})
+
+var tim = document.querySelector('.tecla_tim')
+
+tim.addEventListener("click", function(){
+   document.getElementById('som_tecla_tim').play()
+})
+
+var puff = document.querySelector('.tecla_puff')
+
+puff.addEventListener("click", function(){
+   document.getElementById('som_tecla_puff').play()
+})
+
+var splash = document.querySelector('.tecla_splash')
+
+splash.addEventListener("click", function(){
+   document.getElementById('som_tecla_splash').play()
+})
+
+var toim = document.querySelector('.tecla_toim')
+
+toim.addEventListener("click", function(){
+   document.getElementById('som_tecla_toim').play()
+})
+
+var psh = document.querySelector('.tecla_psh')
+psh.addEventListener("click", function(){
+   document.getElementById('som_tecla_psh').play()
+})
+
+var tic = document.querySelector('.tecla_tic')
+tic.addEventListener("click", function(){
+   document.getElementById('som_tecla_tic').play()
+})
+
+var tom = document.querySelector('.tecla_tom')
+
+tom.addEventListener("click", function(){
+   document.getElementById('som_tecla_tom').play()
+})*/
