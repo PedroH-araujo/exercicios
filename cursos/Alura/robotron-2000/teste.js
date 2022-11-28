792f9703ed97ce6e7f08380e506cb2947b4ef62e
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-11-28 09:42:02 -0300

    update robotron

--- cursos/Alura/robotron-2000/teste.js
+++ cursos/Alura/robotron-2000/teste.js
@@ -1,19 +1,18 @@
-
-const menos = document.querySelector('#subtrair')
-
 const controle = document.querySelectorAll('.controle-ajuste')
 
 controle.forEach((elemento) => {
    elemento.addEventListener("click", (evento) => {
-      manipulaDados(evento.target.textContent)
+      manipulaDados(evento.target.textContent, evento.target.parentNode)
    })
 })
 
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+   const peca = controle.querySelector('.controle-contador')
+   
    if(operacao === "-"){
-      contador.value--
+      peca.value = parseInt(peca.value) - 1
    } else {
-      contador.value++
+      peca.value = parseInt(peca.value) + 1
    }
 }
