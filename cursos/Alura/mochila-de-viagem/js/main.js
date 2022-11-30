eae823e57d374339d613b86e6fd374e95d45839b
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-11-29 21:44:58 -0300

    update

--- cursos/Alura/mochila-de-viagem/js/main.js
+++ cursos/Alura/mochila-de-viagem/js/main.js
@@ -1,37 +1,56 @@
 const form = document.getElementById('novoItem')
 const lista = document.querySelector('.lista')
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach((elemento) => {
+   criaElemento(elemento)
+})
 
 form.addEventListener("submit", (evento) => {
    evento.preventDefault()
 
    const nome = evento.target.elements['nome']
    const quantidade = evento.target.elements['quantidade']
+   
+   const existe =itens.find(elemento => elemento.nome === nome.value)
 
-   criaElemento(nome.value, quantidade.value)
+   const itemAtual = {
+      "nome": nome.value,
+      "quantidade": quantidade.value
+   }
+   
+   if(existe) {
+      itemAtual.id = existe.id
+      console.log(lista.children[existe.id])
+
+      const x = parseInt(lista.children[existe.id].getElementsByTagName('strong')[0].innerHTML) + parseInt(quantidade.value)
+      lista.children[existe.id].getElementsByTagName('strong')[0].innerHTML = x
+   } else {
+      itemAtual.id = itens.length
+
+      criaElemento(itemAtual)
+
+      itens.push(itemAtual)
+   }
+
+   
+   localStorage.setItem("itens", JSON.stringify(itens))
+    
    nome.value = ''
    quantidade.value = ''
 })
 
-function criaElemento(nome, quantidade){
-   const item = document.createElement('li')
-   item.classList.add('item')
+function criaElemento(item){
+   const novoItem = document.createElement('li')
+   novoItem.classList.add("item")
 
    const strong = document.createElement('strong')
-   strong.innerHTML = quantidade
-   item.appendChild(strong)
-   item.innerHTML += nome
+   strong.innerHTML = item.quantidade
+   strong.dataset.id = item.id
+   novoItem.appendChild(strong)
+   
+   novoItem.innerHTML += item.nome
 
-   lista.appendChild(item)
+   lista.appendChild(novoItem)
 
-   const itemAtual = {
-      "nome": nome,
-      "quantidade": quantidade
-   }
-
-   itens.push(itemAtual)
-
-   localStorage.setItem("item", JSON.stringify(itens))
 }
-
-
