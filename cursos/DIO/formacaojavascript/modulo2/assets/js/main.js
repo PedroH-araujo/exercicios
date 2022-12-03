3065734f4287ec3cd29b10f923e5f781798af224
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-03 00:04:57 -0300

    update

--- cursos/DIO/formacaojavascript/modulo2/assets/js/main.js
+++ cursos/DIO/formacaojavascript/modulo2/assets/js/main.js
@@ -1,43 +1,54 @@
+const listaPokemon = document.querySelector('.pokemons')
+const loadMoreButton = document.querySelector('#loadMoreButton')
+const limit = 5
+let offset = 0;
 
 
 
-function convertPokemonToLi(pokemon){
-   return `
-   <li class="pokemon ${pokemon.type}">
-      <span class="number">#${pokemon.number}</span>
-      <span class="name">${pokemon.name}</span>
-      
-      <div class="detail">
-         <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-         </ol>
-         <img src="${pokemon.photo}"   
-          alt="${pokemon.name}">
-      </div>
+
+function loadPokemonItens(offset, limit) {
+   function convertPokemonToLi(pokemon) {
+      return `
+            <li class="pokemon ${pokemon.type}">
+               <span class="number">#${pokemon.number}</span>
+               <span class="name">${pokemon.name}</span>
+               
+               <div class="detail">
+                  <ol class="types">
+                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                  </ol>
+                  <img src="${pokemon.photo}"   
+                  alt="${pokemon.name}">
+               </div>
+         
+         
+            </li>`
+
+   }
 
 
-   </li>`
-   
+
+   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToLi).join('')
+      listaPokemon.innerHTML += newHtml
+   })
 }
 
-const listaPokemon = document.querySelector('.pokemons')
+loadPokemonItens(limit, offset)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-   listaPokemon.innerHTML = pokemons.map(convertPokemonToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+   loadPokemonItens()
 })
 
-  
-
-    
-
-      
-      
-      
-      
-      
 
 
 
-   
-   /*.catch((error) => console.error(error))SE DER ERRO ELE EXECUTA ISSO
-   .finally(() => console.log('Requisição concluída')) DANDO CERTO OU ERRADO, APOS A REQUISIÇÃO ELE EXECUTA*/
+
+
+
+
+
+
+
+/*.catch((error) => console.error(error))SE DER ERRO ELE EXECUTA ISSO
+.finally(() => console.log('Requisição concluída')) DANDO CERTO OU ERRADO, APOS A REQUISIÇÃO ELE EXECUTA*/
