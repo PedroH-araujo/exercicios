c263db8f6ffeae7df8208e3e13e38f838bb53bdf
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-02 22:55:57 -0300

    update

--- cursos/DIO/formacaojavascript/modulo2/assets/js/poke-api.js
+++ cursos/DIO/formacaojavascript/modulo2/assets/js/poke-api.js
@@ -2,22 +2,24 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+   return fetch(pokemon.url)
+   .then((response) => response.json())
+   .then((pokemon) => {
+      
+   })
+}
+
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
 
    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
    return fetch(url)
       .then((response) => response.json())
       .then((jsonBody) => jsonBody.results)
-      .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url).json()))
-      .then((detailRequest))
+      .then((pokemons) => pokemons.map((pokeApi.getPokemonDetail)))
+      .then((detailRequest) => Promise.all(detailRequest))
+      .then((pokemonsDetails) => pokemonsDetails)
       .catch((error) => console.error(error))
 }
-
-Promise.all([
-   fetch('https://pokeapi.co/api/v2/pokemon/1/'),
-   fetch('https://pokeapi.co/api/v2/pokemon/2/'),
-   fetch('https://pokeapi.co/api/v2/pokemon/3/'),
-   fetch('https://pokeapi.co/api/v2/pokemon/4/'),
-]).then((results) => {  //Apos todas essas promessas terminarem ele chama o Then pra mostrar o resultado
-   console.log(results)
-})
