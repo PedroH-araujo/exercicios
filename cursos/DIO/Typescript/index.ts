60e9f4a8046398f95c617076a1c11c3afdc9731c
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-05 21:46:11 -0300

    Projeto banco DIO com typescript finalizado

--- cursos/DIO/Typescript/index.ts
+++ cursos/DIO/Typescript/index.ts
@@ -7,6 +7,7 @@ interface IConta {
 class Conta {
    private nomeTitular: string
    private saldo: number
+   private ativa: boolean = true
 
    constructor({
       nomeTitular,
@@ -17,27 +18,57 @@ class Conta {
       console.log(`Bem vindo ao DIO Bank ${nomeTitular}`)
    }
 
+
    meuSaldo = (): number => this.saldo
    depositar = (valor: number): void => {
-      this.saldo += valor
-      console.log(`Voce depositou ${valor}. Seu novo saldo é ${this.saldo}`)
+      if (!this.ativa) {
+         console.log('Conta desativada,imposível realizar movimentação')
+      } else {
+         this.saldo += valor
+         console.log(`Voce depositou ${valor}. Seu novo saldo é ${this.saldo}`)
+      }
    }
    sacar = (valor: number): void => {
+      if (!this.ativa) {
+         console.log('Conta desativada,imposível realizar movimentação')
+      }
       if (valor <= this.saldo) {
          this.saldo -= valor
          console.log(`Voce sacou ${valor}. Seu novo saldo é ${this.saldo}`)
-      } 
+      } else {
          console.log('Seu saldo é insuficiente')
-      
+      }
    }
+
+   cancelarConta = () => {
+      if (!this.ativa) {
+         console.log('Conta desativada,imposível realizar movimentação')
+      }
+
+      if (this.saldo === 0) {
+         this.ativa = false
+         console.log('Conta desativada')
+      } else if (this.saldo < 0) {
+         console.log(`Ainda existe débitos em sua conta.Seu saldo é ${this.saldo}`)
+      } else {
+         console.log(`Ainda existe créditos em sua conta.Seu saldo é ${this.saldo}`)
+      }
+   }
+
+   transferencia = (valor: number, destinatario: Conta) => {
+      this.saldo -= valor
+      destinatario.saldo += valor
+      console.log(`Voce transferiu ${valor} para a conta de ${destinatario.nomeTitular}`)
+   }
+
 }
 
-const contaA: Conta = new Conta({ nomeTitular: 'Pedro' })
-//const contaB: Conta = new Conta({nomeTitular: 'Peter', saldo: 10})
+const contaA: Conta = new Conta({ nomeTitular: 'Pedro', saldo: 0 })
+const contaB: Conta = new Conta({ nomeTitular: 'Bionda', saldo: 0 })
 
-console.log('Antes do depósito', contaA.meuSaldo())
-contaA.depositar(60)
-console.log('Depois do depósito', contaA.meuSaldo())
-contaA.sacar(70)
-console.log('Depois do saque', contaA.meuSaldo())
-//console.log(contaB)
+
+contaA.depositar(100)
+contaA.transferencia(70,contaB)
+console.log(contaA.meuSaldo())
+console.log(contaB.meuSaldo())
+
