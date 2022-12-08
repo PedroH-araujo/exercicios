699a99fd9c3498624101de0f4a4e4ba5d7e8eeeb
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-08 13:02:55 -0300

    update

--- PMI/main.js
+++ PMI/main.js
@@ -2,6 +2,21 @@
 const menubtn = document.querySelector('#menu-btn')
 const closebtn = document.querySelector('#close-btn')
 const navbar = document.querySelector('#menu-bar')
+const navlist = document.querySelector("#navList")
+
+
+
+window.addEventListener('resize', () => {
+  if(window.getComputedStyle(navlist).display == 'flex'){
+    menubtn.style.display = 'none'
+    closebtn.style.display = 'none';
+    navbar.style.display = 'none';
+  } else {
+    menubtn.style.display = 'inline-block';
+  }
+} )
+
+
 
 menubtn.addEventListener('click', function() {
   navbar.style.display = 'block';
@@ -14,6 +29,7 @@ closebtn.addEventListener('click', () => {
   closebtn.style.display = 'none';
   menubtn.style.display = 'inline-block';
 })
+
 
 
 var swiper = new Swiper(".swiper", {
