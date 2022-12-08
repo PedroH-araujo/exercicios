f237a75b0c7d240eca1b2946454ffa3603a12bc2
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-08 10:51:09 -0300

    conceito avançados de TS

--- cursos/DIO/formacaoangular/aula02/src/index.ts
+++ cursos/DIO/formacaoangular/aula02/src/index.ts
@@ -1,12 +1,38 @@
-//generics
-function concatArray<T>(...itens: T[]): T[]{
-   return new Array().concat(...itens)
+//decorators
+
+function apiVersion(version: string){
+   return (target) => {
+      Object.assign(target.prototype, {__version: version, __name: "Pedro"})
+   }
 }
 
-const numArray = concatArray<number[]>([1,5], [3])
-const stgArray = concatArray<string[]>(["Pedro", "Goku"], ["Vegeta"])
+//attribute decorator
 
-numArray.push("saitama")
+function minLenght(length: number){
+   return (target: any, key: string) => {
+      let _value = console.log(target[key])
 
-console.log(numArray)
-console.log(stgArray)
+      const getter = () => _value
+      const setter = (value: string) => {
+         if(value.length < length) {
+            throw new Error(`Tamanho menor do que ${length}`)
+         } else {
+            _value = value
+         }
+      }
+   }
+}
+
+
+class Api{
+   @minLenght(3)
+   name:string
+
+   constructor(name:string) {
+      this.name = name
+   }
+}
+
+
+const api = new Api("produtos")
+console.log(api.name)
