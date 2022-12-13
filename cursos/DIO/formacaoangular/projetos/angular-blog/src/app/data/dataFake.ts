142082698da526c8359f36bda11ac634bd512747
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-13 15:52:40 -0300

    trabalhando com rotas e deixando a pagina dinâmica

---
+++ cursos/DIO/formacaoangular/projetos/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+   {
+      "id":"1",
+      "photo":"https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2022/05/09142003/Avatar-2-The-Way-of-Water.jpg",
+      "title": "Filme destaque da semana",
+      "description": "Novo filme da franquia Avatar estreia esta semana com gráficos que prometem revolucionar a indústria de filmes"
+   },
+   {
+      "id":"2",
+      "photo":"http://disneyplusbrasil.com.br/wp-content/uploads/2022/10/Pantera-Negra-2-Shuri.jpg",
+      "title": "Pantera Negra 2 conseguiu superar o primeiro filme?",
+      "description": ""
+   }
+]
