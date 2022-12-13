95ba5e66a4fc5b8fc6e362b3dd819028b7ba38cc
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-13 20:14:54 -0300

    update

--- cursos/DIO/formacaoangular/projetos/angular-blog/src/app/data/dataFake.ts
+++ cursos/DIO/formacaoangular/projetos/angular-blog/src/app/data/dataFake.ts
@@ -2,13 +2,49 @@ export const dataFake = [
    {
       "id":"1",
       "photo":"https://d17lbu6bbzbdc8.cloudfront.net/wp-content/uploads/2022/05/09142003/Avatar-2-The-Way-of-Water.jpg",
-      "title": "Filme destaque da semana",
+      "title": "Avatar 2 vem para ser superar o primeiro filme",
       "description": "Novo filme da franquia Avatar estreia esta semana com gráficos que prometem revolucionar a indústria de filmes"
    },
    {
       "id":"2",
       "photo":"http://disneyplusbrasil.com.br/wp-content/uploads/2022/10/Pantera-Negra-2-Shuri.jpg",
       "title": "Pantera Negra 2 conseguiu superar o primeiro filme?",
+      "description": ""
+   },
+   {
+      "id":"3",
+      "photo":"https://serialcookies.com.br/wp-content/uploads/2022/10/Ad%C3%A3o-Negro-04.jpg",
+      "title": "A balança de poder da DC mudou",
+      "description": ""
+   },
+   {
+      "id":"4",
+      "photo":"https://portalpopline.com.br/wp-content/uploads/2022/03/gato-de-botas-2-trailer.jpg",
+      "title": "Filme para todas as idades com diversão garantida",
+      "description": ""
+   },
+   {
+      "id":"5",
+      "photo":"https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2022/10/06/2758267823.jpeg",
+      "title": "Boku no Hero Academia vem para sua 6 temporada",
+      "description": "Na luta contra ALL FOR ONE tivemos uma revelação que ninguem imaginaria"
+   },
+   {
+      "id":"6",
+      "photo":"https://p2.trrsf.com/image/fget/cf/648/0/images.terra.com/2022/11/25/blue-lock-capa-1hrs1zmg672w6.jpg",
+      "title": "Blue Lock vem para ser o anime de esporte do momento",
+      "description": ""
+   },
+   {
+      "id":"7",
+      "photo":"https://i0.wp.com/asianbreak.com.br/wp-content/uploads/2022/03/chainsaw-man.jpg?fit=1200%2C675&ssl=1",
+      "title": "Chainsaw Man vem surpreendendo os fãns",
+      "description": ""
+   },
+   {
+      "id":"8",
+      "photo":"https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/2e54344c7cc1675af721b4021222230f.jpeg",
+      "title": "Quando teremos a volta do melhor anime de todos os tempos",
       "description": ""
    }
 ]
