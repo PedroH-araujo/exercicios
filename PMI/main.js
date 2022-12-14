74936145c6abaa7180dc52014b19865488b9766f
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-14 08:20:01 -0300

    update

--- PMI/main.js
+++ PMI/main.js
@@ -1,10 +1,10 @@
-//MOSTRAR E ESCONDER NAV BAR NO MOBILE
+
 const menubtn = document.querySelector('#menu-btn')
 const closebtn = document.querySelector('#close-btn')
 const navbar = document.querySelector('#menu-bar')
 const navlist = document.querySelector("#navList")
 
-
+//CONFIGURAÇÃO PARA QUANDO ALTERAR O ZOOM DA PAGINA
 window.addEventListener('resize', () => {
   if(window.getComputedStyle(navlist).display == 'flex'){
     menubtn.style.display = 'none';
@@ -17,20 +17,21 @@ window.addEventListener('resize', () => {
   }}
 } )
 
-
+//MOSTRAR NAV BAR NO MOBILE
 menubtn.addEventListener('click', function() {
   navbar.style.display = 'block';
   menubtn.style.display = 'none';
   closebtn.style.display = 'inline-block';
 })
 
+//ESCONDER NAV BAR NO MOBILE
 closebtn.addEventListener('click', () => {
   navbar.style.display = 'none';
   closebtn.style.display = 'none';
   menubtn.style.display = 'inline-block';
 })
 
-
+//SWIPER CONFIG
 var swiper = new Swiper(".swiper", {
    spaceBetween: 30,
    centeredSlides: true,
