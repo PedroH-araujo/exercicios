833a649347e3c8bf7cb071c0984212d1d856867c
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2022-12-29 12:48:41 -0300

    ajustes

--- cursos/ONE/main.js
+++ cursos/ONE/main.js
@@ -38,7 +38,7 @@ descriptografa.addEventListener('click', () => {
 
 
 
-   var str = x[];
+   var str = x[0];
    var chunks = [];
    for (var i = 0, charsLength = str.length; i < charsLength; i += 3) chunks.push(str.substring(i, i + 3));
    for (var i = 0; i < chunks.length; i++) console.log(chunks[i]);
