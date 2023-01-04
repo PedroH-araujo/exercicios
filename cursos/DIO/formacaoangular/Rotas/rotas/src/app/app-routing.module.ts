0032ca1c0b6a7f59c79ce4c9eb40b83f2490a9a0
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-01-03 22:38:44 -0300

    update ROTAS

--- cursos/DIO/formacaoangular/Rotas/rotas/src/app/app-routing.module.ts
+++ cursos/DIO/formacaoangular/Rotas/rotas/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
+
 const routes: Routes = [
-  
+  {path:'', component: TitleComponent, pathMatch:'full'},
+  {path:'portfolio/:id', component: CardComponent},
+  {path:'**', redirectTo:''}
 ];
 
 @NgModule({
