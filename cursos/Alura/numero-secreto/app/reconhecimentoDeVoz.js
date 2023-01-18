52338ae8e53115dbb020ac06d1921551df0dacff
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-01-18 18:05:16 -0300

    feat: finalizando o reconhecimento de voz e implementando uma área para visualizar os chutes

--- cursos/Alura/numero-secreto/app/reconhecimentoDeVoz.js
+++ cursos/Alura/numero-secreto/app/reconhecimentoDeVoz.js
@@ -4,3 +4,60 @@ const recognition = new SpeechRecognition();
 
 recognition.lang = 'pt-Br'
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+const chute = document.querySelector("#chute")
+const num = document.querySelector(".box")
+
+function onSpeak(e) {
+   var palpite = (e.results[0][0].transcript)
+   validaNumero(palpite)
+}
+
+function validaNumero(palpite) {
+   if (palpite < maiorValor && palpite > menorValor) {
+      exibeChuteNaTela(palpite)
+      comparaNumero(palpite)
+      setTimeout(novoPalpite, 1000)
+      exibirPalpites(palpite)
+   } else {
+      alert(`Diga um numero entre ${menorValor} e ${maiorValor}`)
+   }
+}
+
+function exibeChuteNaTela(palpite) {
+   chute.innerHTML = `
+   <div>Você disse:</div>
+   <span class="box">${palpite}</span>
+   `
+}
+
+function novoPalpite() {
+   recognition.start()
+}
+
+function comparaNumero(palpite) {
+   if (palpite == numeroSecreto) {
+      alert("Parabéns voce acertou")
+      chute.innerHTML += `
+   <div>O número secreto é ${numeroSecreto}</div>
+   `
+   }
+   else if (palpite > numeroSecreto) {
+      chute.innerHTML += `
+   <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+   `
+   } else {
+      chute.innerHTML += `
+   <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>
+   `
+   }
+}
+
+const meuspalpites = document.querySelector("#escolhas")
+function exibirPalpites(palpite) {
+   meuspalpites.innerHTML += `
+   <span class="box">${palpite}</span>
+   `
+}
