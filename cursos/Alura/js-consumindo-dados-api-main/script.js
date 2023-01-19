6c6577e1014a44f0dd1aebcb7f1c72c708e3dac3
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-01-19 16:20:51 -0300

    feat: automatizando a insserção do endereço com a API

--- cursos/Alura/js-consumindo-dados-api-main/script.js
+++ cursos/Alura/js-consumindo-dados-api-main/script.js
@@ -1,4 +1,55 @@
-var consultaCEP = fetch ('http://viacep.com.br/ws/01001000/json/')
+const cepform = document.querySelector("#cep")
+const endform = document.querySelector("#endereco")
+const bairroform = document.querySelector("#bairro")
+const cidadeform = document.querySelector("#cidade")
+const estadoselect = document.querySelector("#estado")
+
+
+
+
+cepform.addEventListener('focusout', () => {
+   Requisicao(cepform.value)
+})
+
+async function Requisicao(cep) {
+   try {
+      var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`)
+      var consultaCEPConvertida = await consultaCEP.json()
+      var pegaRua = await consultaCEPConvertida.logradouro
+      var pegaBairro = await consultaCEPConvertida.bairro
+      var pegaCidade = await consultaCEPConvertida.localidade
+      var pegaEstado = await consultaCEPConvertida.uf
+
+      endform.value = pegaRua
+      bairroform.value = pegaBairro
+      cidadeform.value = pegaCidade
+
+      for(var i = 0; i< estadoselect.options.length; i++){
+         if (estadoselect.options[i].value === pegaEstado) {
+            estadoselect.selectedIndex = i;
+            break;
+         }
+      }
+      if (consultaCEPConvertida.erro){
+         throw Error('CEP não existente!')
+      }
+      
+      return consultaCEPConvertida
+   } catch (erro) {
+      console.log(erro);
+   }
+}
+
+
+
+
+
+
+
+
+
+
+/*
 .then(resposta => resposta.json())//COVERTI PARA JSON
 .then(r => {
    if(r.erro) {
@@ -8,5 +59,4 @@ var consultaCEP = fetch ('http://viacep.com.br/ws/01001000/json/')
 .catch(erro => console.log(erro))//MOSTRAR O ERRO NO CONSOLE
 .finally(mensagem => console.log('Processamento conluído')) //INDEPENDENTE DA RESPOSTA ELE ACONTECE
 
-
-console.log(consultaCEP)
+*/
