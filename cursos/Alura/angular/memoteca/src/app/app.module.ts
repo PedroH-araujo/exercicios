81c767c3e0919fb65458a2dae9378432ba27d16c
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-02-03 18:23:31 -0300

    feat: realizando CRUD em Angular

---
+++ cursos/Alura/angular/memoteca/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { CriarPensamentoComponent } from './component/penamentos/criar-pensamento/criar-pensamento.component'
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        CriarPensamentoComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ]
+})
+export class AppModule { }
