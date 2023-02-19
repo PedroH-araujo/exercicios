50b576e928d7012ec2f0f532252bd3b5e95fe8b3
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-02-19 11:59:57 -0300

    feat: mudança de css no header

--- cursos/Alura/angular-testes/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ cursos/Alura/angular-testes/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,10 +1,41 @@
-import { UniqueIDService } from './unique-id.service';
+import { UniqueServiceId } from './unique-id.service';
 
-describe('UniqueIDService', () => {
-  it('generateUniqueIdWhithPrefix should generate id when called with prefix', () => {
-    const service = new UniqueIDService()
+describe(UniqueServiceId.name, () => {
+
+  let service: UniqueServiceId = null
+  beforeEach(() => {
+    service = new UniqueServiceId()
+  })
+
+  it(`#${UniqueServiceId.prototype.generateUniqueIdWhithPrefix.name}
+  should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWhithPrefix('app')
-    expect(id).toContain('app-')
+    expect(id.startsWith('app-')).toBeTrue()
+  })
+
+  it(`#${UniqueServiceId.prototype.generateUniqueIdWhithPrefix.name}
+  should not generate duplicate IDs when called multiple times`, () => {
+    const idArray = []
+    for (let index = 0; index <= 50; index++) {
+      const id = service.generateUniqueIdWhithPrefix('app')
+      idArray.push(id)
+      expect(idArray[index]).not.toBe(idArray[index + 1])
+    }
+  })
+
+  it(`#${UniqueServiceId.prototype.getNumberOfGeneratedUniqueIds.name}
+  should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWhithPrefix('app')
+    service.generateUniqueIdWhithPrefix('app')
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2)
+  })
+
+  it(`#${UniqueServiceId.prototype.generateUniqueIdWhithPrefix.name}
+  should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, '']
+    emptyValues.forEach(emptyValue => {
+      expect(() => service.generateUniqueIdWhithPrefix(emptyValue)).toThrow()
+    })
   })
 
 })
