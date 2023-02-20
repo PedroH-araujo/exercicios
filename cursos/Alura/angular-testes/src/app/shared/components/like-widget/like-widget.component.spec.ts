4ecb8b17ed51cb5621daedea13c8624914ac19f3
Pedro Henrique Vieira Araujo <112811075+PedroH-araujo@users.noreply.github.com>
2023-02-20 19:51:54 -0300

    feat: Iniciando estudos sobre testes em Angular

--- cursos/Alura/angular-testes/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ cursos/Alura/angular-testes/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -4,6 +4,7 @@ import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null
+  let component: LikeWidgetComponent = null
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -11,24 +12,30 @@ describe(LikeWidgetComponent.name, () => {
     }).compileComponents()
 
     fixture = TestBed.createComponent(LikeWidgetComponent)
+    component = fixture.componentInstance
   })
 
   it('Should create component', () => {
-    const instance = fixture.componentInstance
-    expect(instance).toBeTruthy()
+    expect(component).toBeTruthy()
   })
 
-  it('Should auto generate ID when id input property is missing', () => {
-    const component = fixture.componentInstance
+  it('Should auto-generate ID during ngOnInit when (@Input id) is not assigned', () => {
     fixture.detectChanges() // FAZ PASSAR PELO onInit
     expect(component.id).toBeTruthy()
   })
 
-  it('Should NOT generate ID when id input property is present', () => {
-    const component = fixture.componentInstance
+  it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
     const someId = 'someId'
     component.id = someId
     fixture.detectChanges()
     expect(component.id).toBe(someId)
   })
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger (@Output liked) when called`, () => {
+      spyOn(component.liked, 'emit')
+      fixture.detectChanges()
+      component.like()
+      expect(component.liked.emit).toHaveBeenCalled()
+    })
 })
